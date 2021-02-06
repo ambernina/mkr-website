13595d939b1f48c02411f7f8f3bc5b62ad27a68b
ambernina <rob.ambernina@gmail.com>
2021-02-06 15:44:10 -0700

    added switch routes for different pages

--- src/components/AllContent.js
+++ src/components/AllContent.js
@@ -1,19 +1,29 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
+import { Switch, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
 import "../assets/App.css";
 import MKRNav from "./Navbar";
 import MKRJumbotron from "./Jumbotron";
+import HomePage from "../pages/Home";
+import ContactPage from "../pages/Contact";
 
 const AllContent = () => {
 	return (
-    <>
-    <MKRNav />
+		<>
+			<MKRNav />
 			<MKRJumbotron />
-		<Container fluid className="p-3">
-      
-		</Container>
-    </>
+			<Container>
+				<Switch>
+					<Route path="/contact">
+						<ContactPage />
+					</Route>
+					<Route path="/">
+						<HomePage />
+					</Route>
+				</Switch>
+			</Container>
+		</>
 	);
 };
 
