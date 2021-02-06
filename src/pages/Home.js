13a662ae1921f1cb64e129c2584b91e7da3bdfa6
ambernina <rob.ambernina@gmail.com>
2021-02-06 15:50:09 -0700

    changed link to image to actual link instead of local image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,20 @@
 import React from "react";
-import {Container, Row, Col} from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 const HomePage = () => {
-  return (
-    <Container className="p-3">
-        <Row>
-          <Col>
-          <img src={require("../assets/mkr-linkedin-photo.jpeg")} alt="Marla K. Robertson" />
-          </Col>
-          <Col>Paragraph</Col>
-        </Row>
-      </Container>
-  )
-}
+	return (
+		<Container className="p-3">
+			<Row>
+				<Col>
+					<img
+						src="https://media-exp1.licdn.com/dms/image/C4D03AQGw2ZW17Qfurg/profile-displayphoto-shrink_800_800/0/1516923170742?e=1617840000&v=beta&t=EbEriY5u3XXPqW7eoLjRcD0IQh-JDEZFS0fFZWadVVw"
+						alt="Marla K. Robertson"
+					/>
+				</Col>
+				<Col>Paragraph</Col>
+			</Row>
+		</Container>
+	);
+};
 
 export default HomePage;
