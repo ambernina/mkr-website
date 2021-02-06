6db8fe91e2b1254aff655424ba15c373c9c133ec
ambernina <rob.ambernina@gmail.com>
2021-02-06 15:46:57 -0700

    added files for jumbotron dynamic spans

---
+++ src/components/ContactSpan.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ContactSpan = () => {
+  return (
+    <span>Contact</span>
+  )
+}
+
+export default ContactSpan;
---
+++ src/components/HomeSpan.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const HomeSpan = () => {
+  return (
+    <span>About</span>
+  )
+}
+
+export default HomeSpan;
