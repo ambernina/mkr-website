5cb88780c51647aef3e79c1c4215871371bd4ecf
ambernina <rob.ambernina@gmail.com>
2021-02-06 16:12:47 -0700

    moved paragraph to own component and played with downloading cv options

---
+++ src/components/AboutParagraph.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutParagraph = () => {
+	return (
+		<>
+			<span>About Paragraph</span>
+			{/* <a href="./vita-alternate-asof-December-2020.docx" download >Download CV</a> */}
+		</>
+	);
+};
+
+export default AboutParagraph;
