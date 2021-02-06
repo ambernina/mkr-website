a5df8a93f0531f29185e2272d3fe93cf03eec74a
ambernina <rob.ambernina@gmail.com>
2021-02-06 15:45:55 -0700

    added contact page file

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
-const Contact = () => {
-  return (
-    <span>Contact</span>
-  )
-}
+const ContactPage = () => {
+	return (
+		<Container className="p-3">
+			<Row>
+				<Col>Contact Box</Col>
+			</Row>
+		</Container>
+	);
+};
 
-export default Contact;
+export default ContactPage;
