e8bd940efaf675631b1fdf015a7e2facf2dd99b2
ambernina <rob.ambernina@gmail.com>
2021-02-06 15:44:40 -0700

    changed name of component

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,31 +1,26 @@
 import React from "react";
-// import {Route, Switch} from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
 import { Jumbotron } from "react-bootstrap";
 
-// import Home from "../pages/Home";
-// import About from "../pages/About";
-// import Contact from "../pages/Contact";
+import HomeSpan from "../components/HomeSpan";
+import ContactSpan from "../components/ContactSpan";
 
 const MKRJumbotron = () => {
-  return (
-    <Jumbotron>
-				<h1 className="header">Marla K. Robertson</h1>
-				{/* <h2>
-					Current page is{" "}
-					<Switch>
-						<Route path="/about">
-							<About />
-						</Route>
-						<Route path="/contact">
-							<Contact />
-						</Route>
-						<Route path="/">
-							<Home />
-						</Route>
-					</Switch>
-				</h2> */}
-			</Jumbotron>
-  )
-}
+	return (
+		<Jumbotron>
+			<h1 className="header">
+				{' '}
+				<Switch>
+					<Route path="/contact">
+						<ContactSpan />
+					</Route>
+					<Route path="/">
+						<HomeSpan />
+					</Route>
+				</Switch>
+			</h1>
+		</Jumbotron>
+	);
+};
 
 export default MKRJumbotron;
